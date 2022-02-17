b9b514011b4d896ac08e208ba877c6f5b589dc02
Tzuzul Code <mail@tzuzulcode.com>
2022-02-17 16:07:43 -0600

    Juego finlizado

--- pong/lib/pong.dart
+++ pong/lib/pong.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-
+import 'dart:math';
 import 'ball.dart';
 import 'bat.dart';
 
@@ -15,9 +15,12 @@ class Pong extends StatefulWidget {
 class _PongState extends State<Pong> with SingleTickerProviderStateMixin{
   late double width;
   late double height;
+  int score = 0;
   double increment = 10;
-  double posx=0;
-  double posy=0;
+  double posx=1;
+  double posy=1;
+  double randx = 0;
+  double randy= 0;
   double batWidth= 0;
   double batHeight = 0;
   double batPosition = 0;
@@ -37,10 +40,10 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin{
     controller = AnimationController(duration:const Duration(seconds: 100),vsync: this);
     animation = Tween(begin: 0,end:100).animate(controller);
     animation.addListener(() {
-      setState(() {
-        vDir== Direction.down ? posy+=increment: posy-=increment;
-        hDir== Direction.right? posx+=increment: posx-=increment;
-        
+
+      safeSetState((){
+          vDir== Direction.down ? posy+=((increment*randy).round()): posy-=((increment*randy).round());
+          hDir== Direction.right? posx+=((increment*randx).round()): posx-=((increment*randx).round());
       });
       checkBorders();
     });
@@ -56,6 +59,11 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin{
       batWidth = width / 5;
       batHeight = height / 25;
       return Stack(children:[
+        Positioned(
+          child: Text("Score:"+score.toString()),
+          top: 10,
+          right: 10,
+          ),
         Positioned(
           child: Ball(),
           top: posy,
@@ -74,23 +82,83 @@ class _PongState extends State<Pong> with SingleTickerProviderStateMixin{
   }
 
   void moveBat(DragUpdateDetails update){
-    setState(() {
+    safeSetState(() {
       batPosition += update.delta.dx;
     });
   }
 
   void checkBorders(){
+    double diameter = 50;
     if(posx<=0 && hDir==Direction.left){
       hDir = Direction.right;
+      randx = randomNumber();
     }
-    if(posx>=width && hDir==Direction.right){
+    if(posx>=width-diameter && hDir==Direction.right){
       hDir = Direction.left;
+      randx = randomNumber();
     }
     if(posy<=0 && vDir==Direction.up){
       vDir = Direction.down;
+      randy = randomNumber();
     }
-    if(posy>=height && vDir==Direction.down){
-      vDir = Direction.up;
+    if(posy>=height-diameter -batHeight && vDir==Direction.down){
+      if(posx>=(batPosition-diameter-25) && posx<=(batPosition+batPosition+diameter+25)){
+        vDir = Direction.up;
+        randx = randomNumber();
+        safeSetState((){
+          score++;
+        });
+      }else{
+        controller.stop();
+        showMessage(context);
+      }
+    }
+  }
+
+  void showMessage(BuildContext context){
+    showDialog(
+      context: context, 
+      builder: (BuildContext context){
+        return AlertDialog(
+          title: Text("Game Over"),
+          content: Text("Wold you like to play again?"),
+          actions: [
+            TextButton(
+              onPressed: (){
+                setState(() {
+                  posx =0;
+                  posy=0;
+                  score=0;
+                });
+                Navigator.of(context).pop();
+                controller.repeat();
+              }, 
+              child: Text("Yes")
+              ),
+              TextButton(
+              onPressed: (){
+                Navigator.of(context).pop();
+                dispose();
+              }, 
+              child: Text("No")
+              )
+          ],
+        );
+      });
+  }
+
+  double randomNumber(){
+    var ran = new Random();
+    double myNum = ran.nextDouble();
+
+    return myNum+0.5;
+  }
+
+  void safeSetState(Function function){
+    if(mounted && controller.isAnimating){
+      setState(() {
+        function();
+      });
     }
   }
 
